02ef3f0454d7118ba642dcc6691a1d35c0a201e5
jefre <jefredujon@gmail.com>
2025-09-30 09:14:33 -0400

    first commit

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class  Hello {
+
+    public static void main(String[] args) {
+
+        // call the method sayHello
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+
+    }
+
+    private static void sayGoodMorning() {
+        System.out.println("Good Morning");
+    }
+
+    private static void sayGoodbye() {
+        System.out.println("Goodbye");
+
+    }
+
+    private static void sayHello() {
+        System.out.println("Hello World");
+    }
+}
+
