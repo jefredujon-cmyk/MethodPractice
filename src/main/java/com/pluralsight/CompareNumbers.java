843953bbcdf4a9adeabfec076a52b90baa576610
jefre <jefredujon@gmail.com>
2025-09-30 10:26:53 -0400

    first commit

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -6,9 +6,10 @@ public class CompareNumbers {
 
         System.out.println(isEven(  5));
         System.out.println(isEven(  4 ));
+        System.out.println(isPositive(  0));
     }
 
-        public static boolean isEven(int number){
+    public static boolean isEven(int number){
 
             if(number % 2 ==0){
                 return true;
@@ -17,6 +18,14 @@ public class CompareNumbers {
             return false;
     }
 
+    public static boolean isPositive(double number){
+        if( number > 0) {
+            return true;
+        }else{
+            return false;
+
+        }
+    }
 
 }
 
